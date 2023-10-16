3c9cf331530292a741be5446353be76950af06ad
Tynar Esenbaeva <esenbaevat@yahoo.com>
2023-10-16 08:05:18 -0400

    add the code

--- README.md
+++ README.md
@@ -1,1 +1,96 @@
-# challenge_food_truck_cli
+# Food Truck Locator Command Line Interface (CLI) application
+
+Thank you for taking some time to review the code. I embarked on my journey to learn my very first programming language, Python, just two weeks ago. I'm looking forward to discussing my work in person, sharing my additional ideas, and explaining my approach to tackling this challenge. If you are new with challenges and python as well, this the beginner friendly  start to solve this challenge.
+
+
+This is a Python script that allows you to interact with a CSV file containing data about food trucks. You can perform various operations to search for, list, and find information about food trucks based on different criteria.
+
+
+## Getting Started
+
+To run this script, you'll need to set up a Python environment, install the necessary packages, and have a CSV file with food truck data.
+
+### Prerequisites
+
+- Python 3.7+
+- Virtual Environment (venv) - To create an isolated Python environment.
+- geopy - A Python library for calculating distances between coordinates.
+
+### Installation
+
+1. Clone this repository to your local machine:
+
+    ```bash
+    git clone https://github.com/tikenek/challenge_food_truck_cli.git
+    ```
+
+2. Create a virtual environment:
+
+    ```bash
+    cd challenge_food_truck_cli
+    python -m venv venv
+
+    #On Mac use:
+    source venv/bin/activate
+
+    # On Windows, use: 
+    .venv\Scripts\activate
+    # On Linux, use:
+    source venv/bin/activate
+    ```
+
+3. Install the required packages using pip:
+
+    ```bash
+    pip install geopy
+    ```
+
+### Usage
+
+The script provides the following commands for interacting with food truck data:
+
+First you need to run "python3 app.py" in the project directory to start the CLI
+
+1. **List All Food Trucks:**
+
+
+
+    ```bash
+    list
+    ```
+
+2. **Search for Specific Food Trucks:**
+
+    ```bash
+    search
+    ```
+
+3. **Get Information About Food Trucks in a Specific Address:**
+
+    ```bash
+    address
+    ```
+
+4. **Calculate Distance to Food Trucks:**
+
+    ```bash
+    distance
+    ```
+
+5. **Find Nearby Food Trucks:**
+
+    ```bash
+    nearby
+    ```
+
+6. **Exit the Program:**
+
+    ```bash
+    exit
+    ```
+
+Make sure to answer questions after you type one of the above commands when prompted.
+
+## Acknowledgments
+
+- This script uses the [geopy](https://geopy.readthedocs.io/) library for geospatial calculations.
---
+++ app.py
@@ -0,0 +1,153 @@
+import csv
+import argparse
+from geopy.distance import geodesic
+
+
+
+csv_file_name = "Mobile_Food_Facility_Permit.csv"
+
+
+# List all food trucks in the CSV file
+def list_food_trucks(file_path):
+    with open(file_path, 'r') as csvfile:
+        csvreader = csv.DictReader(csvfile)
+        for row in csvreader:
+            print("Applicant:", row['Applicant'])
+            print("Food Items:", row['FoodItems'])
+            print("Address:", row["Address"])
+            print("Location:", row['Location'])
+            print("=" * 30)
+
+
+
+# Function to search for specific types of food trucks
+def search_food_trucks(file_path, food_type):
+    with open(file_path, 'r') as csvfile:
+        csvreader = csv.DictReader(csvfile)
+        found_trucks = []
+        for row in csvreader:
+            if food_type.lower() in row['FoodItems'].lower():
+                found_trucks.append(row)
+            elif food_type.lower() in row["Applicant"].lower():
+                found_trucks.append(row)
+
+        if found_trucks:
+            print(f"Food Trucks serving {food_type}:")
+            for truck in found_trucks:
+                print("Applicant:", truck['Applicant'])
+                print("Food Items:", row['FoodItems'])
+                print("Location:", truck['Location'])
+                print("=" * 30)
+        else:
+            print(f"No food trucks found serving {food_type}.")
+
+
+# Function to provide information about food trucks in a specific address
+def location_info(file_path, location):
+    with open(file_path, 'r') as csvfile:
+        csvreader = csv.DictReader(csvfile)
+        found_trucks = []
+        for row in csvreader:
+            if location.lower() in row['Address'].lower():
+                found_trucks.append(row)
+
+        if found_trucks:
+            print(f"Food Trucks in {location}:")
+            for truck in found_trucks:
+                print("Applicant:", truck['Applicant'])
+                print("Food Items:", truck['FoodItems'])
+                print("Address:", truck["Address"])
+                print("=" * 30)
+        else:
+            print(f"No food trucks found in {location}.")
+
+
+
+# Function to calculate the distance between two coordinates
+def calculate_distance(coord1, coord2):
+    return geodesic(coord1, coord2).meters
+
+# Function to find and display nearby food trucks
+def find_nearby_food_trucks(file_path, location, max_distance):
+    with open(file_path, 'r') as csvfile:
+        csvreader = csv.DictReader(csvfile)
+        nearby_trucks = []
+        for row in csvreader:
+            truck_location = (float(row['Latitude']), float(row['Longitude']))
+            distance = calculate_distance(location, truck_location)
+            if distance <= max_distance:
+                nearby_trucks.append(row)
+
+        if nearby_trucks:
+            print(f"Nearby Food Trucks within {max_distance} meters:")
+            for truck in nearby_trucks:
+                truck_location = (float(truck['Latitude']), float(truck['Longitude']))
+                distance = calculate_distance(location, truck_location)
+                print("Applicant:", truck['Applicant'])
+                print("Food Items:", truck["FoodItems"])
+                print("Address:", truck["Address"])
+                print("Location:", truck['Location'])
+                print("Distance:", distance, "meters")
+                print("=" * 30)
+        else:
+            print(f"No food trucks found within {max_distance} meters of the specified location.")
+
+
+
+
+
+# Main function to handle user interactions
+def main():
+    while True:
+        print("\nAvailable commands:")
+        print("1. list - List all food trucks")
+        print("2. search - Search for specific food trucks")
+        print("3. address - Get information about food trucks in an address")
+        print("4. distance - Calculate the distance to food trucks")
+        print("5. nearby - Find nearby food trucks")
+        print("6. exit - Exit the program")
+        
+        user_input = input("Enter a command: ").strip()
+
+        if user_input == "list":
+            list_food_trucks(csv_file_name)
+        elif user_input == "search":
+            food_type = input("Enter the type of food to search for: ").strip()
+            search_food_trucks(csv_file_name, food_type)
+        elif user_input == "address":
+            location = input("Enter the address to search for (For example: California): ").strip()
+            location_info(csv_file_name, location)
+        elif user_input == "distance":
+            latitude = float(input("Enter latitude: "))
+            longitude = float(input("Enter longitude: "))
+            max_distance = float(input("Enter maximum distance (meters): "))
+            # Calculate the distance from a specified location to all food trucks
+            if latitude is not None and longitude is not None:
+                user_location = (latitude, longitude) # Specify the user's location (latitude, longitude)
+                with open(csv_file_name, 'r') as csvfile:
+                    csvreader = csv.DictReader(csvfile)
+                    nearby_trucks = []
+                    for row in csvreader:
+                        truck_location = (float(row['Latitude']), float(row['Longitude']))
+                        distance = calculate_distance(user_location, truck_location)
+                        # Display only the truck and its distance if it is within the specified distance
+                        if distance <= max_distance:
+                            print("Applicant:", row['Applicant'])
+                            print("Food Items:", row["FoodItems"])
+                            print("Address:", row["Address"])
+                            print("Location:", row['Location'])
+                            print("Distance:", distance, "meters")
+                            print("=" * 30)
+        elif user_input == "nearby":
+            latitude = float(input("Enter latitude: "))
+            longitude = float(input("Enter longitude: "))
+            max_distance = float(input("Enter maximum distance (meters): "))
+            user_location = (latitude, longitude)
+            find_nearby_food_trucks(csv_file_name, user_location, max_distance)
+        elif user_input == "exit":
+            break
+        else:
+            print("Invalid command. Please choose a valid command from the docs.")
+
+if __name__ == "__main__":
+    main()
