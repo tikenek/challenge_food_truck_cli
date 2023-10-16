2ff9840e3d778c7ca6aaa1bd72fcfbe9f92b4502
Tynar Esenbaeva <esenbaevat@yahoo.com>
2023-10-16 08:41:36 -0400

    update

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def location_info(file_path, location):
 
 # Function to calculate the distance between two coordinates
 def calculate_distance(coord1, coord2):
-    return geodesic(coord1, coord2).miles
+    return geodesic(coord1, coord2).meters
 
 # Function to find and display nearby food trucks
 def find_nearby_food_trucks(file_path, location, max_distance):
